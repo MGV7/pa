e7f29aacc82e0da2ac888df071a86fb9e53f2c9b
MGV_7 <miggbrito@gmail.com>
2026-03-25 12:32:33 -0300

    Add files via upload

---
+++ testemb/src/testemb/Testemb.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testemb;
+
+/**
+ *
+ * @author Admin
+ */
+public class Testemb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=5, b=10, soma=0, divisão=0, subtração=0, multiplicação=0;
+                soma=a+b;
+                divisão=b/a;
+                subtração=b-a;
+                multiplicação=a*b;
+                System.out.println ("A soma d:" +soma);
+                System.out.println ("A soma d:" +divisão);
+                System.out.println ("A soma d:" +subtração);
+                System.out.println ("A soma d:" +multiplicação);
+    }
+    
+}
